46178d9f7fb9895d20673b68d194c37d229ca105
AndyLocks <jandylokc@gmail.com>
2023-10-21 23:14:45 +0200

    )

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.car.MegaEngine;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import org.example.car.Car;
@@ -8,7 +9,7 @@ import org.example.car.Car;
 public class Main {
     public static void main(String[] args) {
 
-        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")){
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class)){
             Car car = context.getBean("car", Car.class);
             System.out.println(car.getEngine().getName());
             System.out.println(car.getEngine().getSpeed());
